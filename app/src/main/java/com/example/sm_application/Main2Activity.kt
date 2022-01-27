7f279a2ad9e981f292ff8cb018e7e3eefd7ec7bc
Tigran <tiko02220@gmail.com>
2022-01-27 20:18:59 +0400

    i

---
+++ app/src/main/java/com/example/sm_application/Main2Activity.kt
@@ -0,0 +1,56 @@
+package com.example.sm_application
+
+import android.os.Bundle
+import android.view.Menu
+
+import android.support.design.widget.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.AppCompatActivity
+import com.example.sm_application.databinding.ActivityMain2Binding
+
+class Main2Activity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain2.toolbar)
+
+     binding.appBarMain2
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main2)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,R.id.adminFragment
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main2, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main2)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
+
+
---
+++ app/src/main/java/com/example/sm_application/ui/home/AdminFragment.kt
@@ -0,0 +1,25 @@
+package com.example.sm_application.ui.home
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sm_application.R
+
+class AdminFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AdminFragment()
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.admin_fragment, container, false)
+    }
+
+
+}
